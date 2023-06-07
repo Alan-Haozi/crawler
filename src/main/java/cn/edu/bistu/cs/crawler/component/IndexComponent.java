6c1790042a74363b23fadbe7fcbb4dbf8027e004
刘浩辉 <729682816@qq.com>
2023-06-07 22:45:10 +0800

    5.14

--- src/main/java/cn/edu/bistu/cs/crawler/component/IndexComponent.java
+++ src/main/java/cn/edu/bistu/cs/crawler/component/IndexComponent.java
@@ -8,10 +8,11 @@ import org.springframework.stereotype.Component;
 public class IndexComponent {
     /**
      * 把索引对象转化成文档对象
+     *
      * @param htmlIndex 索引类对象
      * @return 一个Document(一行内容)
      */
-    public Document toDoc(HtmlIndex htmlIndex){
+    public Document toDoc(HtmlIndex htmlIndex) {
         // 1 创建文档对象
         Document document = new Document();
         // 创建并添加字段信息
@@ -20,14 +21,13 @@ public class IndexComponent {
         // TextField, 既创建索引又会被分词
         // 参数：字段的名称、字段的值、是否存储，Store.YES代表存储到文档列表。Store.NO代表不存储
         document.add(new StringField("id", String.valueOf(htmlIndex.getId()), Field.Store.YES));
-        document.add(new StringField("username", htmlIndex.getUsername(),Field.Store.YES));
+        document.add(new StringField("username", htmlIndex.getUsername(), Field.Store.YES));
         document.add(new TextField("title", htmlIndex.getTitle(), Field.Store.YES));
         document.add(new TextField("content", htmlIndex.getContent(), Field.Store.NO)); // 不带标签的html
         return document;
     }
 
     /**
-     *
      * @param doc 一个Document(一行内容)
      * @return htmlIndex 索引类对象
      */
